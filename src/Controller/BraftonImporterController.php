18eb55a589bd5714af444a9e0c2f8545e90a0db9
unknown <edwardhornig@gmail.com>
2015-11-17 10:56:44 -0500

    fix menu link; remove service from controller

--- src/Controller/BraftonImporterController.php
+++ src/Controller/BraftonImporterController.php
@@ -14,60 +14,16 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class BraftonImporterController extends ControllerBase {
 
-  protected $braftonImporterService;
-
   /**
-   * Class constructor
+   * Provides the Brafton config form.
+   *
+   * @return object $form The Brafton config form.
    */
-  public function __construct($braftonImporterService) {
-    $this->braftonImporterService = $braftonImporterService;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('brafton_importer.brafton_importer_service')
-    );
-  }
-
   public function content() {
-
     $form = \Drupal::formBuilder()->getForm('Drupal\brafton_importer\Form\BraftonForm');
-
-  //  $form['#attached']['library'][] = 'brafton_importer/brafton_css';
-  //  debug($form);
-
     return $form;
-
-
-/*
-    $test = \Drupal::entityManager()->getStorage('field_storage_config')->load('field_brafton_image');
-    $test2 = 'yeah buddy';
-    debug($test);
-    return array(
-      '#markup' => t('Hello @value', array(
-        '@value' => $test
-      ))
-    );
-*/
-
-    /*
-    return array(
-      '#type' => 'markup',
-      '#markup' => t('Hello world'),
-    );
-    */
-/*
-    return array(
-      '#markup' => t('Hello @value', array(
-        '@value' => $this->braftonImporterService->getDemoValue()
-      ))
-    );
-*/
-
   }
+
 }
 
 ?>
